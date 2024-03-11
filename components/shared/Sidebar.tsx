25202835558eddbc067f1fcadab9961a27cf8a8f
Deepanshu <deepanshu.kewalramani2@gmail.com>
2024-03-11 18:31:33 +0530

    changed the banner and the logo

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -12,9 +12,9 @@ const Sidebar = () => {
 
   return (
     <aside className="sidebar">
-      <div className="flex size-full flex-col gap-4">
+      <div className="flex size-full flex-col gap-4 items-center">
         <Link href="/" className="sidebar-logo">
-          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+          <Image src="/assets/images/pixel-perfect-logo.png" alt="logo" width={200} height={28} />
         </Link>
 
         <nav className="sidebar-nav">
